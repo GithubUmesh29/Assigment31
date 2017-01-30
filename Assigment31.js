ac9302903aaffceeb4d517c0db4fa35cdfcda846
Umesh Rameshwar Brahmankar <jaiurb@rediffmail.com>
2017-01-31 00:38:17 +0530

    This assignment will test your skills on ES6 we learnt in the class.
    Create an array and pass in a function as rest parameter, then ● Find out length of array using getArrLength() ● Multiply by 2 to all element and display new array using multiplyByTwo() ● Sort rest arguments using sortRestArgs() ● Create an object apiDetail which should contain: ● baseUrl ● Method ● Request api parameter (i.e. /user) ● Header  ● Post data if method is post  Pass that object in function where make an API call. 
     Note: keep in mind we have to use ES6 feature to solve this problem.

--- Assigment31.js
+++ Assigment31.js
@@ -6,34 +6,48 @@
 ● Sort rest arguments using sortRestArgs() 
 */
 
+var materials = [ 8,9,2,4,5];
+
+function restParameter(a,b,c,d,e){
+	console.log('Length of Array ');
+	getArrLength( a,b,c,d,e );
+	console.log('');
+	console.log('Array multiply by two');
+	multiplyByTwo(2, a,b,c,d,e );
+	console.log('');
+	console.log('Array in sorted order');
+	sortRestArgs( a,b,c,d,e );
+}
+
+
 function getArrLength(...theArgs) {
   console.log(theArgs.length);
 }
 
-getArrLength();  // 0
-getArrLength(5); // 1
-getArrLength(5, 6, 7); // 3
+// getArrLength();  // 0
+// getArrLength(5); // 1
+// getArrLength(5, 6, 7); // 3
 
 
 
 function multiplyByTwo(multiplier, ...theArgs) {
-  return theArgs.map(function (element) {
+  console.log( theArgs.map(function (element) {
     return multiplier * element;
-  });
+  }));
 }
 
-var arr = multiplyByTwo(2, 1, 2, 3); 
-console.log(arr); // [2, 4, 6]
+// var arr = multiplyByTwo(2, 1, 2, 3); 
+// console.log(arr); // [2, 4, 6]
 
 
 
 function sortRestArgs(...theArgs) {
   var sortedArgs = theArgs.sort();
-  return sortedArgs;
+  console.log(sortedArgs);
 }
 
-console.log(sortRestArgs(5,3,7,1));
+// console.log(sortRestArgs(5,3,7,1));
 
-
+restParameter( ...materials );
 
 
